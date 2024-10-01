e90e51a7f81b7dd1a7bbc2311074d623b1f1a872
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-10-01 14:17:10 +0500

    Implemented router and added login and signup pages in (auth) using Next.js

---
+++ 04. day-four/src/app/(auth)/login/page.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <div className="content flex items-center justify-center h-screen">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+          <h2 className="text-2xl font-bold mb-6 text-gray-900 text-center">Login</h2>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
